7883d79f288bc420fc7f629459476cd7c95e28f0
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-08-18 11:26:31 -0300

    Update vetorController.java

--- Exerc-02/vetorController.java
+++ Exerc-02/vetorController.java
@@ -1,4 +1,4 @@
-package vetorRecursivo;
+package controller;
 
 import javax.swing.*;
 /**
@@ -14,22 +14,27 @@ public class vetorController {
     public int[] geraVetor(int n) {
         int[] vet = new int[n];
         for (int i = 0; i < n; i++){
+//          Escrever um valor de termo n para cada posição do Vetor
             vet[i] = Integer.parseInt(JOptionPane.showInputDialog("Insira um numero inteiro para preencher o vetor:"));
         }
         return vet;
     }
     
-    public int contaNegaVet(int[] vet, int contador, int size) {
-            if (size == 0) {
+    public int contaNegaVet(int[] vet, int size) {
+//          condição de parada
+        if (size == 0) {
                 return 0;
-            } else {
-                int lastPst = size - 1;
-                int valor = vet[lastPst];
-                if (valor < 0) {
-                    contador++;
-                }
+        } else {
+            int lastPst = size - 1;
+            int valor = vet[lastPst];
+            if (valor < 0) {
                 size -= 1;
-                return contaNegaVet(vet, contador, size);
+                return 1 + contaNegaVet(vet, size);
+                //  retorna size em função do termo anterior, se negativo
+            }
+            size -= 1;
+            return contaNegaVet(vet, size);
+            //  retorna size em função do termo anterior, se positivo
             }
     }
 }
