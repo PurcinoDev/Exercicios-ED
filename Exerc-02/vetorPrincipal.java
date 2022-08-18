0e4cafa34195ce59b9343c97a66638dac8c5568f
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-08-18 11:27:32 -0300

    Update vetorPrincipal.java

--- Exerc-02/vetorPrincipal.java
+++ Exerc-02/vetorPrincipal.java
@@ -1,4 +1,4 @@
-package vetorRecursivo;
+package view;
 
 import javax.swing.*;
 /**
@@ -7,15 +7,14 @@ import javax.swing.*;
  */
 public class vetorPrincipal {
     public static void main(String[] args) {
-        int contador = 0;
         
         vetorController vetCon = new vetorController();
         int n = Integer.parseInt(JOptionPane.showInputDialog("Insira um numero para definir o tamanho do vetor"));
         int[] vetGer = vetCon.geraVetor(n);
         
         int size = vetGer.length;
-        int resultado = vetCon.contaNegaVet(vetGer, contador, size);
+        int resultado = vetCon.contaNegaVet(vetGer, size);
         
-        System.out.println("A quantidade de numeros negativos neste vetor e de: " + resultado);
+        JOptionPane.showMessageDialog(null, "A quantidade de numeros negativos neste vetor e de: " + resultado);
     }
 }
